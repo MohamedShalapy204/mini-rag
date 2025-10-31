b1a07918ff47cea974564de53559fc3d7f34a56b
MohamedShalapy204 <mohamedshalapy7919@gmail.com>
2025-10-31 22:58:32 +0200

    create pgvector provider

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -1,10 +1,11 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import List
 
 class Settings(BaseSettings):
     
     APP_NAME: str
     APP_VERSION: str
-    # OPENAI_API_KEY: str
+    OPENAI_API_KEY: str
 
     FILE_ALLOWED_TYPES: list
     FILE_MAX_SIZE: int
@@ -17,19 +18,21 @@ class Settings(BaseSettings):
     POSTGRES_MAIN_DATABASE: str
 
     GENERATION_BACKEND: str
-    EMBEDDING_BACKEND:str
+    EMBEDDING_BACKEND: str
 
-    OPENAI_API_KEY:str = None
-    OPENAI_API_URL:str = None
-    COHERE_API_KEY:str = None
+    OPENAI_API_KEY: str = None
+    OPENAI_API_URL: str = None
+    COHERE_API_KEY: str = None
 
-    GENERATION_MODEL_ID:str = None
-    EMBEDDING_MODEL_ID:str = None
-    EMBEDDING_MODEL_SIZE:int = None
-    INPUT_DAFAULT_MAX_CHARACTERS:int = None
-    GENERATION_DAFAULT_MAX_TOKENS:int = None
-    GENERATION_DAFAULT_TEMPERATURE:float = None
+    GENERATION_MODEL_ID_LITERAL: List[str] = None
+    GENERATION_MODEL_ID: str = None
+    EMBEDDING_MODEL_ID: str = None
+    EMBEDDING_MODEL_SIZE: int = None
+    INPUT_DAFAULT_MAX_CHARACTERS: int = None
+    GENERATION_DAFAULT_MAX_TOKENS: int = None
+    GENERATION_DAFAULT_TEMPERATURE: float = None
 
+    VECTOR_DB_BACKEND_LITERAL: List[str] = None
     VECTOR_DB_BACKEND: str
     VECTOR_DB_PATH: str
     VECTOR_DB_DISTANCE_METHOD: str = None
